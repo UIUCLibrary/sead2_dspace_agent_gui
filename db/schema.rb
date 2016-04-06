b56ff7c0175c12df6a5a6bd9f688f51344dc7822
Nushrat Jahan Khan <njkhan2@illinois.edu>
2016-04-06 15:53:50 -0500

    update scss with library header and footer design

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160406193109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deposits", force: :cascade do |t|
+    t.string   "email"
+    t.string   "title"
+    t.string   "author"
+    t.date     "creation_date"
+    t.string   "project_url"
+    t.string   "status"
+    t.string   "state"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
